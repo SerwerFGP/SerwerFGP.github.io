ab8075f0e45163c7c46bdffb21fdd566b1f96b17
SerwerFGP <77665076+SerwerFGP@users.noreply.github.com>
2021-02-06 17:57:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+let dscUsername;
+
+window.onload = () => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+
+    if(fragment.has('access_token')) {
+        const accessToken = fragment.get("access_token");
+		const tokenType = fragment.get("token_type");
+
+        fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${tokenType} ${accessToken}`;
+            }
+        }).then(res => res.json())
+        .then(response => {
+            const { username, discriminator } = response;
+
+            dscUsername = `${username}#${discriminator}`;
+        }).catch(console.error);
+    }else {
+        document.getElementById('login').style.display = 'block';
+    }
+}
+
 const app = new Vue({
     el: '#app',
     data: {
       isVisible: true,
-      msg: 'ezzz',
+      msg: dscUsername,
     },
     methods: {
         toggle() {
