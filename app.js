42069b6e83233d3f10be410bfdb818961f7f2ff9
SerwerFGP <77665076+SerwerFGP@users.noreply.github.com>
2021-02-28 14:22:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-document.onload = () => {
+window.onload = () => {
     alert('Strona jest w fazie tworzenia.');
 }
 
 function mobileState() {
     const mobileNav = document.getElementById('nav');
+    const logoMobile = document.getElementById('logo-mobile');
 
     if(mobileNav.style.marginLeft == '-50%') {
         mobileNav.style.marginLeft = '0';
+        logoMobile.style.opacity = '0';
     }
     else {
         mobileNav.style.marginLeft = '-50%';
+        logoMobile.style.opacity = '1';
     }
 }
